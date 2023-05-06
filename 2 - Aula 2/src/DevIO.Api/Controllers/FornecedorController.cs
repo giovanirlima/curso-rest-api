7d0a7658e1a321a0cd0f5989623593c63dc39e28
giovanirlima <Watzeckdev@gmail.com>
2023-05-06 17:13:09 -0300

    criado fornecedorController, baseController

---
+++ 2 - Aula 2/src/DevIO.Api/Controllers/FornecedorController.cs
@@ -0,0 +1,41 @@
+﻿using DevIO.Business.DataTransferObjects;
+using DevIO.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    [ApiController]
+    public class FornecedorController : BaseApiController
+    {
+        private readonly IFornecedorService _services;
+
+        public FornecedorController(IFornecedorService services) =>
+            _services = services;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<FornecedorDTO>>> ObterTodosAsync()
+        {
+            return Ok(await _services.ObterTodosAsync());
+        }
+
+        [HttpGet("{id:Guid}")]
+        public async Task<ActionResult<FornecedorDTO>> ObterPorIdAsync(Guid id)
+        {
+            var fornecedor = await _services.ObterPorIdAsync(id);
+
+            if (fornecedor == null)
+                return NotFound(new
+                {
+                    Sucess = false,
+                    Notification = "Fornecedor não encontrato."
+                });
+
+            return Ok(new
+            {
+                Sucess = true,
+                Data = fornecedor
+            });
+        }
+    }
+}
